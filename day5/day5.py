d4dfc687e1ed0a58025cec3a42915b71243c8244
Quan <lhquan05@gmail.com>
2026-08-29 10:28:36 +0200

    in ten va tuoi position

---
+++ day5/day5.py
@@ -0,0 +1,109 @@
+def main():
+    student_list=["Quan","Phuc","Tuan"]
+    
+    student_list.append("HP")
+    
+    Ten="Hải"
+    
+    student_list.append(Ten)
+    
+    print(len(student_list))
+    
+    student_list.append({'Ten'  : 'Quan',
+                         'Tuoi' : '27'})
+    
+    student_list.append(27)
+    
+    student_list.append(["Nhu","Thong"])
+    
+    student_list.append(2.5)
+    
+    
+    print(student_list)
+    
+    for name in student_list:
+        print(name)
+        
+def day5():
+
+    bien = ("phuc97", "Phuc")
+    
+    quan=(2)
+    
+    bien_mat=()
+    
+    print(type(bien_mat))
+    
+
+    print(type(quan))
+    
+    print(type(bien))
+    
+    
+    a=("quan", 5, 2.5 )
+    
+    # a.insert(1, "Phuc" )  Tuple ko có
+    
+    # a.append("hai") Tuple ko có
+    
+    # a.remove("quan")
+    
+    employee_list=[{'Ten': "A1",
+                   'Tuoi' :"20",
+                   'Position':""},
+                   {'Ten': "A2",
+                   'Tuoi' :"20",
+                   'Position':""},
+                   {'Ten': "A3",
+                   'Tuoi' :"20",
+                   'Position':""},
+                   {'Ten': "A4",
+                   'Tuoi' :"20",
+                   'Position':""},
+                   {'Ten': "A5",
+                   'Tuoi' :20,
+                   'Position':""}]
+    
+    
+    
+    a=employee_list[0]
+    for x, y in a.items():
+        print(c)
+        
+    
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    postion=("giám đốc","accountant","manager","leader","nhân viên")
+    
+    for each_person in employee_list:
+       
+        print(each_person)
+    
+    
+                  
+    
+    
+    
+    
+    
+    
+    for b in a:
+        print(b)
+        
+    
+    
+    
+    
+
+
+        
+if __name__ == "__main__":
+    # main()
+    day5()
