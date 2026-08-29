8d91042acef60d77e0540ae9d80d2829d41e456b
Phuc Le Quang <phuc.lequang001@stud.fh-dortmund.de>
2026-08-29 10:56:48 +0200

    answer and exe

--- day5/day5.py
+++ day5/day5.py
@@ -48,60 +48,76 @@ def day5():
     
     # a.remove("quan")
     
-    employee_list=[{'Ten': "A1",
-                   'Tuoi' :"20",
-                   'Position':""},
-                   {'Ten': "A2",
-                   'Tuoi' :"20",
-                   'Position':""},
-                   {'Ten': "A3",
-                   'Tuoi' :"20",
-                   'Position':""},
-                   {'Ten': "A4",
-                   'Tuoi' :"20",
-                   'Position':""},
-                   {'Ten': "A5",
-                   'Tuoi' :20,
-                   'Position':""}]
-    
-    
-    
-    a=employee_list[0]
-    for x, y in a.items():
-        print(c)
-        
-    
-        
-        
-    
-    
-    
-    
-    
-    
+    employee_list=[{
+            'Ten': "A1",
+            'Tuoi' :"20",
+            'Position':""
+        },
+        {
+            'Ten': "A2",
+            'Tuoi' :"20",
+            'Position':""
+        },
+        {
+            'Ten': "A3",
+            'Tuoi' :"20",
+            'Position':""
+        },
+        {
+            'Ten': "A4",
+            'Tuoi' :"20",
+            'Position':""
+        },
+        {
+            'Ten': "A5",
+            'Tuoi' :20,
+            'Position':""
+        }]
     
     postion=("giám đốc","accountant","manager","leader","nhân viên")
     
+    #step 1: Lay cai dict ra từ List
+    #Step 2: IN key của cái element vừa lấy ra
+    #Step 3: Gán positions
+    
+    nv_1 = employee_list[0]
+    nv_2 = employee_list[1]
+    nv_3 = employee_list[2]
+    nv_4 = employee_list[3]
+    nv_5 = employee_list[4]
+    
+    print(nv_1['Ten'])
+    print(nv_1['Position'])
+    
+    pos_nv1 = nv_1['Position']  #Lấy position từ nv1 ra gán vào biến pos_nv1
+    
+    """
+    nv_1['Position'] = postion[0] # Lấy tuple ra gán vào dict
+    nv_2['Position'] = postion[1] # Lấy tuple ra gán vào dict
+    nv_3['Position'] = postion[2] # Lấy tuple ra gán vào dict
+    nv_4['Position'] = postion[3] # Lấy tuple ra gán vào dict
+    nv_5['Position'] = postion[4] # Lấy tuple ra gán vào dict
+    """
+    
+    print(nv_1['Position'])
+    
+    print("Gan POS  cho Nhan Vien")
+    
+    i = len(postion) - 1
+    for nv in employee_list:
+        a = nv['Position']              # Lấy vị trí của nhân viên đó 
+        nv['Position'] =  postion[i]    # tupple thứ i 
+        i = i - 1
+    
+    
+    # for x, y in nv_1.items():
+    #     print(x,y)
+    
+    ################################################################
     for each_person in employee_list:
        
         print(each_person)
     
-    
-                  
-    
-    
-    
-    
-    
-    
-    for b in a:
-        print(b)
-        
-    
-    
-    
-    
-
 
         
 if __name__ == "__main__":
---
+++ day5/day5e.py
@@ -0,0 +1,8 @@
+# Reuse the employee list and position from day 5
+# Do the following requirements:
+#  - Tạo biến current_year = 2026.
+#  - người A2 nghĩ việc, xoá tên họ khỏi cty 
+#  - Năm mới => Mỗi người tăng lên 1 tuổi. Chỉ khi current_year = 2027
+#  - Position cũng tăng lên theo. Ex: Nhân viên -> Leader..... , 
+#  - There should be two giam doc in the company
+#  - Print the whole company's list.
