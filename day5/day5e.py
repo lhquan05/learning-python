f1a0bfa587d11f73134530e3d812b43480d9f24a
Phuc Le Quang <phuc.lequang001@stud.fh-dortmund.de>
2026-09-04 23:32:36 +0200

    ex

--- day5/day5e.py
+++ day5/day5e.py
@@ -3,6 +3,13 @@
 #  - Tạo biến current_year = 2026.
 #  - người A2 nghĩ việc, xoá tên họ khỏi cty 
 #  - Năm mới => Mỗi người tăng lên 1 tuổi. Chỉ khi current_year = 2027
+#  - In ra câu "Chuc mung sinh nhat lan thu Tuoi cua Ten"
 #  - Position cũng tăng lên theo. Ex: Nhân viên -> Leader..... , 
 #  - There should be two giam doc in the company
 #  - Print the whole company's list.
+
+
+
+
+
+
